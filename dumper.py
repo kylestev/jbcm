ab8be282759993d1e4d6195093d00526598a1aa4
Kyle Stevenson <kyle.stevenson94@gmail.com>
2013-03-03 21:21:12 -0800

    Fix invalid member of ConstantPool

--- dumper.py
+++ dumper.py
@@ -228,7 +228,7 @@ class ConstantPool:
         item = self.pool[index]
 
         if item.tag_id == 7:
-            return self.index[item.value['type']].get_value()
+            return self.pool[item.value['type']].get_value()
         elif item.tag_id == 12:
             return {'name': self.get(item.value['name']).get_value(),
                     'type': self.get(item.value['type']).get_value()}
