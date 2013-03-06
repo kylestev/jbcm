cb7f811922815984c34c2370e077f62ea42693dc
Kyle Stevenson <kyle.stevenson94@gmail.com>
2013-03-05 18:15:03 -0800

    Add documentation to the Reader class

--- reader.py
+++ reader.py
@@ -4,19 +4,36 @@ class Reader:
     pool = None
 
     def read_byte(self):
+        """
+        Reads a single byte from the buffer
+        """
+
         self.pos += 1
         return ord(self.buff[self.pos])
 
     def read_short(self):
+        """
+        Reads a short, or two bytes, from the buffer
+        """
+
         return (self.read_byte() << 8) + self.read_byte()
 
     def read_int(self):
+        """
+        Reads an integer, or four bytes, from the buffer
+        """
+
         return (self.read_short() << 16) + self.read_short()
 
     def read_long(self):
         return (self.read_int() << 32) + self.read_int()
 
     def read(self, length):
+        """
+        Reads a chunk with the specified length and returns it
+        @param length the size of the chunk to read in
+        """
+        
         b = []
 
         for i in range(length):
@@ -26,5 +43,11 @@ class Reader:
         return b
 
     def load_class(self, file):
+        """
+        Convenience method for setting the buffer of this instance to a file's
+        contents
+        @param file the file to read in
+        """
+
         with open(file, 'rb') as f:
             self.buff = f.read()
